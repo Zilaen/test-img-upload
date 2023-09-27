c046fe8b3bbcf50766d873f7b4515d17d9f1811b
Silent <anvasmos@gmail.com>
2023-09-27 16:03:00 +0300

    WIP: Image Download (BP) and Class Stub (With Errors)

--- Source/ImageUploaderTest/MyClass.cpp
+++ Source/ImageUploaderTest/MyClass.cpp
@@ -1,12 +1,50 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "MyClass.h"
+#include "IImageWrapper.h"
+#include "IImageWrapperModule.h"
+#include "Runtime/Online/HTTP/Public/Http.h"
 
-MyClass::MyClass()
+UMyClass::UMyClass()
 {
+
 }
 
-MyClass::~MyClass()
+void UMyClass::LoadImageFromURL(const FString& URL)
 {
+    TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+    HttpRequest->OnProcessRequestComplete().BindUObject(this, &UMyClass::OnImageDownloaded);
+
+    HttpRequest->SetURL(URL);
+    HttpRequest->SetVerb(TEXT("GET"));
+    HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("image/jpeg"));
+
+    HttpRequest->ProcessRequest();
+}
+
+void UMyClass::OnImageDownloaded(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded)
+{
+    if (bSucceeded && HttpResponse.IsValid() && HttpResponse->GetResponseCode() == EHttpResponseCodes::Ok)
+    {
+        TArray<uint8> ResponseData = HttpResponse->GetContent();
+
+        IImageWrapperModule& ImageWrapperModule = FModuleManager::LoadModuleChecked<IImageWrapperModule>(FName("ImageWrapper"));
+        TSharedPtr<IImageWrapper> ImageWrapper = ImageWrapperModule.CreateImageWrapper(EImageFormat::JPEG);
+
+        if (ImageWrapper.IsValid() && ImageWrapper->SetCompressed(ResponseData.GetData(), ResponseData.Num()))
+        {
+            const TArray<uint8>* RawData = nullptr;
+            if (ImageWrapper->GetRaw(ERGBFormat::BGRA, 8, RawData))
+            {
+                UTexture2D* Texture = UTexture2D::CreateTransient(ImageWrapper->GetWidth(), ImageWrapper->GetHeight(), PF_B8G8R8A8);
+                if (Texture)
+                {
+                    void* TextureData = Texture->PlatformData->Mips[0].BulkData.Lock(LOCK_READ_WRITE);
+                    FMemory::Memcpy(TextureData, RawData->GetData(), RawData->Num());
+                    Texture->PlatformData->Mips[0].BulkData.Unlock();
+                    Texture->UpdateResource();
+
+                    // TODO: use texture
+                }
+            }
+        }
+    }
 }
--- Source/ImageUploaderTest/MyClass.h
+++ Source/ImageUploaderTest/MyClass.h
@@ -1,15 +1,22 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "MyClass.generated.h"
 
 /**
- * 
+ *
  */
-class IMAGEUPLOADERTEST_API MyClass
+UCLASS()
+class IMAGEUPLOADERTEST_API UMyClass : public UObject
 {
+    GENERATED_BODY()
+
 public:
-	MyClass();
-	~MyClass();
+    UMyClass();
+
+    void LoadImageFromURL(const FString& URL);
+
+protected:
+    void OnImageDownloaded(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded);
 };
